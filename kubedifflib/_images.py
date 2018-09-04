weaveworks/kubediff

commit 74cff4aedd33cd7c2b90e66031722606d2a0a635

Fix #24: add support for .yml extension in addition to .yaml

kubedifflib/_images.py
@@ -21,7 +21,8 @@ def load_config(*paths):
   """
   objects = {}
   for filename in iter_files(paths):
-    if not filename.endswith('.yaml'):
+    _, extension = os.path.splitext(path)
+    if extension not in [".yaml", ".yml"]:
       continue
     with open(filename, 'r') as stream:
       data = yaml.load(stream)

kubedifflib/_diff.py
@@ -229,7 +229,7 @@ def check_files(paths, printer, kubeconfig=None):
   differences = 0
   for path in iter_files(paths):
     _, extension = os.path.splitext(path)
-    if extension != ".yaml":
+    if extension not in [".yaml", ".yml"]:
       continue
     differences += check_file(printer, path, kubeconfig=kubeconfig)
 

commit 4de89c2253cc8757fd9b2078d956286b0839ae23

Fix undefined name 'path'
The error was first introduced in https://github.com/weaveworks/kubediff/commit/74cff4ae

This change improves consistency between _images.py and _diff.py.

kubedifflib/_images.py
@@ -23,11 +23,11 @@ def load_config(*paths):
   :return: a dict mapping KubeObjects to data.
   """
   objects = {}
-  for filename in iter_files(paths):
+  for path in iter_files(paths):
     _, extension = os.path.splitext(path)
     if extension not in [".yaml", ".yml"]:
       continue
-    with open(filename, 'r') as stream:
+    with open(path, 'r') as stream:
       data = yaml.load(stream)
     kube_obj = KubeObject.from_dict(data)
     objects[kube_obj] = data